d6a2e690a8a2c61fa3fd72100fa5a7e0c91a0071
gyshim <gyshim1004@gmail.com>
2024-11-05 01:27:13 +0900

    1st day

--- app/src/main/java/eu/tutorial/myfirstapp/MainActivity.kt
+++ app/src/main/java/eu/tutorial/myfirstapp/MainActivity.kt
@@ -13,12 +13,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import eu.tutorial.myfirstapp.ui.theme.MyFirstAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity : ComponentActivity() {      // 액티비티는 사용자 화면
+    override fun onCreate(savedInstanceState: Bundle?) {        // 실행될 때 실행
+        super.onCreate(savedInstanceState)      // 먼저 실행하세요
         enableEdgeToEdge()
         setContent {
-            MyFirstAppTheme {
+            MyFirstAppTheme {   // modifier은 composable의 외관 수정, 제어
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -30,15 +30,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
+@Composable     // composable은 ui요소 -> 초록색으로 표시
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Welcome $name!",
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true)     //  에뮬하지 않고 수정 내용 확인하기 위함
 @Composable
 fun GreetingPreview() {
     MyFirstAppTheme {
